3642fdac4496889e221ae8814befe0c9c45da33c
Felipe Streva <fstreva@poli.ufrj.br>
2020-09-30 09:53:45 -0300

    .gitignore de DB

--- database/DAO/client/clientDAO.js
+++ database/DAO/client/clientDAO.js
@@ -7,8 +7,17 @@ class ClientDAO{
     getAllClientsInDB(){
         return new Promise((resolve, reject) => {
             this._db.all(`SELECT * FROM client`, [], (err, rows) => {
-                if(err) reject(`Error in the query: ${err}`)
+                if(err) reject(`Error in SELECT Query: ${err}`)
                 resolve(rows)
+            })
+        })
+    }
+
+    deleteClientInDB(id){
+        return new Promise((resolve, reject) => {
+            this._db.run(`DELETE FROM client WHERE client_id = ?`, [id], (err) => {
+                if(err) reject(`Error in DELETE Query: ${err}`)
+                resolve(`Client deleted`)
             })
         })
     }
