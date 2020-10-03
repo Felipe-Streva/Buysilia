6f0e12b62e3c148e7c27023745811ecc05195af8
Victoria Marques <vicck.rj@gmail.com>
2020-10-02 22:16:35 -0300

    feat: add insert client in db

--- database/DAO/client/clientDAO.js
+++ database/DAO/client/clientDAO.js
@@ -22,6 +22,21 @@ class ClientDAO{
         })
     }
 
+    insertClientInDB(body) {
+        return new Promise( (resolve, reject) => {
+            const INSERT = `
+                INSERT INTO client (
+                    first_name, last_name, email, password, cpf, phone, adress
+                ) VALUES (?, ?, ?, ?, ?, ?, ?);
+            `;
+            const params = [body.first_name, body.last_name, body.email, body.password, body.cpf, body.phone, body.adress]; 
+            this._db.run(INSERT, params, (err) => {
+                if(err) reject(`Error in INSERT Query: ${err}`)
+                resolve(`Client inserted`)
+            })
+        })
+    }
+
     deleteClientInDB(id){
         return new Promise((resolve, reject) => {
             this._db.run(`DELETE FROM client WHERE client_id = ?`, [id], (err) => {
