69dfdb875ba4325c4c605f69d056b22be2f3397d
Felipe Streva <fstreva@poli.ufrj.br>
2020-10-02 22:39:26 -0300

    fix  bugs

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,12 +1,7 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
-
-app.use(bodyParser.json())
-app.use(bodyParser.text())
-
 
 module.exports = app
